ab6968a9b29a6f6aabc9f051222d4b234e7dfd76
Rebanko <rost20003rri@gmail.com>
2023-10-12 04:52:47 +0500

    Initial commit

---
+++ app/src/main/java/com/example/laba1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.laba1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+
+    }
+}
